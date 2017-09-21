83b8984da819f62471c33467fed415d28c5763ec
Ekaterina Ledeneva <31830048+cthfvrl@users.noreply.github.com>
2017-09-22 01:59:19 +0300

    More permutation stuff

--- store.h
+++ store.h
@@ -1,39 +1,55 @@
-#ifndef STORE
-#define STORE
+#ifndef STORE_H
+#define STORE_H
 #include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include "gray.h"
 
 using namespace std;
 
 class Store {
 	static const int N = 3;
 	struct Box {
+		int    num;
 		double len[N];
 
-		Box(vector<double>x = vector<double>(N, 0));
+		Box();
+		Box(vector<double>x);
 		friend istream& operator>>(istream &is,
 		                           Box     &b);
+		friend bool     operator<(const Box &l,
+		                          const Box &r);
 		double&         operator[](int n);
+		double          operator[](int n) const;
+		void init();
+		void next();
+		void prev();
 		~Box();
 	};
 
 	friend istream& operator>>(istream &is,
 	                           Box     &b);
+	friend bool     operator<(const Box &l,
+	                          const Box &r);
+
 	int n;
 	Box space;
 	vector<Box> boxes;
+	Gray<6> gray;
 
 	bool check();
+	void init();
+	void next();
 
 public:
 
 	Store();
-	void            read(const string &filename = "");
+	void            read(const string &filename);
 	friend istream& operator>>(istream &is,
 	                           Store   &s);
 	bool            fits();
 	~Store();
 };
 
-#endif // ifndef STORE
+#endif // ifndef STORE_H
